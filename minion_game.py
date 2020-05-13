110bc222a1a4b5475a558b53752265a63edba7f7
Abhishek <abhishekchoudhary2k17@gmail.com>
2020-05-14 01:18:23 +0530

    Update and rename minion_game to minion_game.py

--- minion_game.py
+++ minion_game.py
@@ -1,35 +1,35 @@
-Kevin and Stuart want to play the 'The Minion Game'.
+#Kevin and Stuart want to play the 'The Minion Game'.
 
-Game Rules
+#Game Rules
 
-Both players are given the same string,S.
-Both players have to make substrings using the letters of the string S.
-Stuart has to make words starting with consonants.
-Kevin has to make words starting with vowels.
-The game ends when both players have made all possible substrings.
+#Both players are given the same string,S.
+#Both players have to make substrings using the letters of the string S.
+#Stuart has to make words starting with consonants.
+#Kevin has to make words starting with vowels.
+#The game ends when both players have made all possible substrings.
 
-Scoring
-A player gets +1 point for each occurrence of the substring in the string S.
+#Scoring
+#A player gets +1 point for each occurrence of the substring in the string S.
 
-For Example:
-String S= BANANA
-Kevin's vowel beginning word = ANA
-Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
+#For Example:
+#String S= BANANA
+#Kevin's vowel beginning word = ANA
+#Here, ANA occurs twice in BANANA. Hence, Kevin will get 2 Points.
 
-Your task is to determine the winner of the game and their score.
+#Your task is to determine the winner of the game and their score.
 
-Input Format
-A single line of input containing the string .
-Note: The string  will contain only uppercase letters: .
+#Input Format
+#A single line of input containing the string .
+#Note: The string  will contain only uppercase letters: .
 
-Constraints
-0<len(S)<=10^6
+#Constraints
+#0<len(S)<=10^6
 
-Output Format
-Print one line: the name of the winner and their score separated by a space.
-If the game is a draw, print Draw.
+#Output Format
+#Print one line: the name of the winner and their score separated by a space.
+#If the game is a draw, print Draw.
 
-##CODE##
+#CODE
 def minion_game(string):
     length=len
     stuw= list()
